32ab815523889cb20af3bb23a0677e67e6f9703f
Eric S <estriler@hotmail.com>
2024-08-20 18:48:56 -0500

    finished react part 2 exercise

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,12 @@
 import './styles.css';
 
 function Button() {
+   function onLearnMore(){
+      alert("Splishy Splashy!")
+   }
 
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
