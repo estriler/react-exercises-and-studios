d9cd36b73cf07b9bed5943ea05214e07dfb06630
Eric S <estriler@hotmail.com>
2024-08-24 13:42:59 -0500

    this is working now and im not sure why???

---
+++ part3/shoe-counting-exercise/shoe-counting-button/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ShoeButton from './components/ShoeButton'
+import ReportForm from './components/Report'
+
+function App() {
+
+  return (
+    <>
+      <ShoeButton />
+      <ReportForm />
+    </>
+  )
+}
+
+export default App
